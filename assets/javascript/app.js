576e6d398f93e4d2132cb7c1e415e20fac6fa37c
Laura Unaeze <l.unaeze5@gmail.com>
2017-08-18 18:16:55 -0700

    Started working on JS logic.

---
+++ assets/javascript/app.js
@@ -0,0 +1,47 @@
+//variables
+var remainingTime = 30; //as in 30 seconds
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unanswered = 0;
+
+//The questions for now.
+//I will add more later when everything comes together.
+var questionOne = {
+	question: "What is generally considered the start of the work week in the United States?",
+	choices: ["Sunday", "Monday", "Friday", "Saturday"],
+	correct: "Monday"
+};
+
+var questionTwo = {
+	question: "What month was I born in?",
+	choices: ["January", "December", "June", "July"],
+	correct: "July"
+};
+
+console.log(questionTwo.question);   //test
+console.log(questionTwo.choices[3]); //test
+
+
+$(document).ready(function () {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
